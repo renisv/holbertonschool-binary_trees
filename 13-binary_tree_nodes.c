7d80d8ff4d2ab627e4f11263e8ee24c414e7198c
renisv <11432@holbertonstudents.com>
2025-07-29 09:59:47 +0000

    update

---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+    size_t left_internal = 0;
+    size_t right_internal = 0;
+    
+    if (tree == NULL)
+        return (0);
+    
+    if(tree->left == NULL && tree->right == NULL)
+        return (0);
+
+    if (tree->left != NULL)
+        left_internal = binary_tree_nodes(tree->left);
+
+    if (tree->right != NULL)
+        right_internal = binary_tree_nodes(tree->right);
+    
+    return (left_internal + right_internal + 1);
+}
